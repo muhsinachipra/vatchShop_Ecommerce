efe724cc167700536933e63dfcd07db3a4ccc9e5
Muhsin Achipra <muhsinachipra@gmail.com>
2023-10-28 18:11:02 +0530

    user template (coza-store) added 3

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -24,15 +24,21 @@ const path= require("path")
 
 const userController = require("../controllers/userController");
 
-user_route.get('/',auth.isUserLogout,userController.loginLoad)
-user_route.post('/',userController.verifyLogin)
-user_route.get('/register',auth.isUserLogout,userController.loadRegister);
-user_route.post('/register',userController.insertUser);
-user_route.get('/otp',auth.isUserLogout,userController.loadOtp);
-user_route.post('/otp', userController.verifyOTP)
-user_route.post('/resendOTP', userController.resendOTP)
-user_route.get('/userHome',auth.isUserLogin,userController.loadHome)
-user_route.get('/logout',auth.isUserLogin,userController.userLogout)
+// user_route.get('/',auth.isUserLogout,userController.loginLoad)
+// user_route.post('/',userController.verifyLogin)
+// user_route.get('/register',auth.isUserLogout,userController.loadRegister);
+// user_route.post('/register',userController.insertUser);
+// user_route.get('/otp',auth.isUserLogout,userController.loadOtp);
+// user_route.post('/otp', userController.verifyOTP)
+// user_route.post('/resendOTP', userController.resendOTP)
+// user_route.get('/logout',auth.isUserLogin,userController.userLogout)
+// user_route.get('/userHome',auth.isUserLogin,userController.loadHome)
+
+
+user_route.get('/',auth.isUserLogout,userController.loadHome)
+user_route.get('/login',auth.isUserLogout,userController.loginLoad)
+
+
 
 
 module.exports=user_route
