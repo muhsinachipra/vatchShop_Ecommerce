4263f18d991da11aac709cd3e9a76770e3449754
Muhsin Achipra <muhsinachipra@gmail.com>
2024-09-06 20:09:26 +0530

    error handling

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -1,29 +1,28 @@
 const isAdminLogin = async (req, res, next) => {
-    try {
-      if (req.session.admin_id) {
-        next(); // User is logged in, proceed to the next middleware or route
-      } else {
-        res.redirect('/admin'); // User is not logged in, redirect to login page
-      }
-    } catch (error) {
-      next(error);
+  try {
+    if (req.session.admin_id) {
+      next(); // User is logged in, proceed to the next middleware or route
+    } else {
+      res.redirect('/admin'); // User is not logged in, redirect to login page
     }
+  } catch (error) {
+    next(error);
   }
-  
-  const isAdminLogout = async (req, res, next) => {
-    try {
-      if (req.session.admin_id) {
-        res.redirect('/admin/dashboard'); // User is logged in, redirect to home
-      } else {
-        next(); // User is logged out, proceed to the next middleware or route
-      }
-    } catch (error) {
-      next(error);
+}
+
+const isAdminLogout = async (req, res, next) => {
+  try {
+    if (req.session.admin_id) {
+      res.redirect('/admin/dashboard'); // User is logged in, redirect to home
+    } else {
+      next(); // User is logged out, proceed to the next middleware or route
     }
+  } catch (error) {
+    next(error);
   }
-  
-  module.exports = {
-    isAdminLogout,
-    isAdminLogin
-  }
-  
+}
+
+module.exports = {
+  isAdminLogout,
+  isAdminLogin
+}
